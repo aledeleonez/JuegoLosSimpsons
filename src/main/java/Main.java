f53039b3a610f2556c1a634e6be70ec3ac4105ad
Silvia Lorero Salor <1004291182alumnos.uc3m.es>
2023-01-15 12:37:42 +0100

    Página principal empezada

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,12 @@
+import javax.swing.*;
+
 public class Main {
+    private JButton btnRankin;
+    private JButton btnJugar;
+    private JPanel JPanel;
+    private JButton salirButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
