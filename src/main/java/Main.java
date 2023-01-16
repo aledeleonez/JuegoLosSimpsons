ec5a84b980f6f4095cc36e1349c4514e9faeb1c0
Alejandro de León Fernández <aledeleonez@gmail.com>
2023-01-16 10:21:09 +0000

    Panel de personajes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,13 @@
 import javax.swing.*;
+import java.awt.*;
 
-public class Main {
+public class Main extends javax.swing.JFrame{
     private JButton btnRankin;
     private JButton btnJugar;
     private JPanel JPanel;
     private JButton salirButton;
 
+    
     private void createUIComponents() {
         // TODO: place custom component creation code here
     }
